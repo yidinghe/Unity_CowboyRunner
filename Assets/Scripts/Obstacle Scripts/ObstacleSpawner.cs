166b76be340e89e11cefcb2c1ead3fdb879818b3
yiding he <yidinghe2012@gmail.com>
2018-03-13 11:48:06 -0400

    Added ObstacleSpawer and Collector.
    TODO: Script continue.

---
+++ Assets/Scripts/Obstacle Scripts/ObstacleSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+
+	[SerializeField]
+	private GameObject[] obstacles;
+
+	private List<GameObject> obstaclesForSpawing = new List<GameObject> ();
+
+
+	void Awake ()
+	{
+		
+	}
+
+	void InitializeObstacles ()
+	{
+		int index = 0;
+		for (int i = 0; i < obstacles.Length * 3; i++) {
+			GameObject obj = Instantiate (obstacles [index], transform.position, Quaternion.identity) as GameObject;
+			obstaclesForSpawing.Add (obj);
+			obstaclesForSpawing [i].SetActive (false);
+			index++;
+			if (index == obstacles.Length)
+				index = 0;
+		}
+	}
+}
