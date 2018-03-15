f899dd7d0a45dde71e2ff0cb5c5dfd9e3294a829
yiding he <yidinghe2012@gmail.com>
2018-03-14 22:06:02 -0400

    Added MainMenu.
    TODO: GamePlay Controller logic.

---
+++ Assets/Scripts/GameController Scripts/MainMenuController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+
+
+	public void PlayGame ()
+	{
+		SceneManager.LoadScene ("GamePlay");
+	}
+		
+
+}
